1a582dcb7f88e9803ba134ef5425b68efc647c45
joranegs92 <joranegs92@gmail.com>
2023-06-29 20:13:31 +0900

    서블렛 로그인] 간단하게 로그인페이지만 구축

--- commerceServlet/src/main/java/com/example/commerceServlet/HelloServlet.java
+++ commerceServlet/src/main/java/com/example/commerceServlet/HelloServlet.java
@@ -1,9 +1,12 @@
 package com.example.commerceServlet;
 
 import java.io.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
+/*
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
 	private String message;
@@ -24,4 +27,38 @@ public class HelloServlet extends HttpServlet {
 
 	public void destroy() {
 	}
+}
+*/
+@WebServlet("/login")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		UserService.addUser(new User("123", "123"));
+
+		User user = UserService.getUser(username);
+		if (user != null && user.getPassword().equals(password)) {
+			// 로그인 성공
+			out.println("<h1>로그인대성공</h1>");
+			HttpSession session = request.getSession();
+			session.setAttribute("username", username);
+
+			//로그인페이지로 이동
+			RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			// 로그인 실패
+			out.println("<h1>로그인대실패</h1>");
+		}
+		out.println("</body></html>");
+	}
+
 }
