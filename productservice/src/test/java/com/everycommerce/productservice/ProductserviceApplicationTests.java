4de3dee04bd5098f8c69d3dbcf785c7aed1edde6
joranegs92 <joranegs92@gmail.com>
2023-09-10 19:42:45 +0900

    productService추가] msa 두번째 프로젝트 생성

---
+++ productservice/src/test/java/com/everycommerce/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everycommerce.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
