204d29d16394d6fa7b9d6c7e00ac30a001794961
joranegs92 <joranegs92@gmail.com>
2023-09-14 00:51:38 +0900

    ProductService] docker수정 , 기본설정수정, spring security 추가

---
+++ productservice/src/main/java/com/everycommerce/productservice/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.everycommerce.productservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+/*		http.authorizeRequests(a ->{
+			try {
+				a.
+			}catch (Exception e){
+				throw new RuntimeException(e);
+			}
+		});*/
+		http.csrf(c ->{
+			try{
+				c.ignoringAntMatchers("/");
+			}catch (Exception e){
+				throw new RuntimeException(e);
+			}
+		});
+		return http.build();
+	}
+
+
+}
---
+++ productservice/src/main/java/com/everycommerce/productservice/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.everycommerce.productservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+
+
+}
