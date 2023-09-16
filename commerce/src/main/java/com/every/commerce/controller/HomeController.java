52cad5c23b733a3267733dc492fecbac0df50b64
joranegs92 <joranegs92@gmail.com>
2023-09-16 22:08:57 +0900

    gateway]yml파일 수정, 로그인부분과 연결 :아직 진행안됨

--- commerce/src/main/java/com/every/commerce/controller/HomeController.java
+++ commerce/src/main/java/com/every/commerce/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/member-service")
 public class HomeController {
 	@RequestMapping("/")
 	public String home() {
