bae59a322994e71848186e29e679663a7154efc8
joranegs92 <joranegs92@gmail.com>
2023-09-18 17:07:51 +0900

    gateway]product, memberservice 연결 완료

--- productservice/src/main/java/com/everycommerce/productservice/controller/ProductController.java
+++ productservice/src/main/java/com/everycommerce/productservice/controller/ProductController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/product-service")
 public class ProductController {
 	@RequestMapping("/")
 	public String home() {
-		return "hello";
+		return "forward:/hello.html";
 	}
 }
